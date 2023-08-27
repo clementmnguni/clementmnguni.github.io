7a7afe83f5030681b10c255539d05e0e2bc22892
clementmnguni <102917816+clementmnguni@users.noreply.github.com>
2023-08-27 19:49:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -201,7 +201,7 @@ document.addEventListener('DOMContentLoaded', () => {
       cardsChosenId = []
       resultDisplay.textContent = score +((cardsWon.length+1)-cardslost.length)
       if  (cardsWon.length === cardArray.length/2) {
-        resultDisplay.textContent = 'Congratulations Nkosie! You found them all!'
+        resultDisplay.textContent = 'Congratulations! You found them all!'
       }
     }
   
